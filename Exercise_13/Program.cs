453c9570bb841d781c615a83ee0ca48107c64ba2
Sergei <shkulev555@gmail.com>
2023-05-01 08:31:37 +0300

    initial commit

---
+++ Exercise_13/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int number = userNumber;
+int quantityNumber = 0;
+int result = 0;
+
+
+// Determine the number of digits
+if (number != 0)
+{
+    while (number >= 1 || number <= -1)
+    {
+        number /= 10;
+        quantityNumber++;
+
+    } 
+}
+else quantityNumber = 1;
+
+
+// search third digital
+if (quantityNumber >= 3)
+{
+    while (quantityNumber >= 3)
+    {
+        result = userNumber % 10;
+        userNumber /= 10;
+        quantityNumber -= 1;
+    }
+
+    Console.Write($"{Math.Abs(result)} - третья цифра ");
+}
+else
+{
+    Console.Write("третьей цифры нет ");
+}
