2d0801873640c8489c26bf2c7b5cdb403532383e
Sergei <shkulev555@gmail.com>
2023-05-01 08:48:45 +0300

    initial commit

---
+++ Exercise_10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+userNumber /= 10;
+result = userNumber % 10;
+
+
+Console.Write($"{Math.Abs(result)} - вторая цифра");
